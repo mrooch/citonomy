cc11c4a55e5deb04c1fdbc4147295c3924f7af22
mohammad <mb@ringel.co>
2017-03-29 14:30:00 +0430

    Initial commit

---
+++ cite-storage/src/main/java/Application.java
@@ -0,0 +1,10 @@
+/**
+ * Author: mohammad
+ * Creation Date: 3/29/2017
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
