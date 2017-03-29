b55483b3d223a82538ffd3fec6dc04df942e218b
mohammad <mb@ringel.co>
2017-03-29 16:54:29 +0430

    moved controllers in com.citonomy package

--- cite-hub/src/main/java/com/citonomy/ServiceFacade.java
+++ cite-hub/src/main/java/com/citonomy/ServiceFacade.java
@@ -1,3 +1,5 @@
+package com.citonomy;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
